84db962ee3d1821be8e8d98cb9af4feb7e7d9f65
Arancha <afa028@gmail.com>
2026-04-15 20:24:33 +0200

    fix: modified ports/repositories/index.ts

--- src/application/ports/repositories/index.ts
+++ src/application/ports/repositories/index.ts
@@ -1,4 +1,3 @@
 export type { IPlaylistRepository } from './PlaylistRepository'
 export type { IToolRepository } from './ToolRepository'
-export type { IPracticeRepository } from './PracticeRepository'
 export type { VideoRepository } from './VideoRepository'
